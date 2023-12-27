c93cdb4d48ba34fd3e7c4ca8418768c77bf021a5
Anupa Isuru <anupaisuru2002@gmail.com>
2023-12-27 13:47:32 +0530

    create sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
+      <Home/>
     </div>
   );
 }
