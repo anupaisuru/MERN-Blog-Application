99fc7c25e610397edb6cd8d67487c27334b97186
Anupa Isuru <anupaisuru2002@gmail.com>
2023-12-27 20:00:44 +0530

    create register and login pages.

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Settings/>
+      <Register/>
     </div>
   );
 }
