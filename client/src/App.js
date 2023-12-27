367b629e6a593c90dffd1efc2fb1a1dd99a5ae6d
Anupa Isuru <anupaisuru2002@gmail.com>
2023-12-27 18:12:17 +0530

    create new post component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <div className="App">
       <Topbar />
-      <Single/>
+      <Write/>
     </div>
   );
 }
