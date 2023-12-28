83fc619c7e70137bda75201987ae2088417cdb0f
Anupa Isuru <anupaisuru2002@gmail.com>
2023-12-28 12:27:11 +0530

    create update,delete user api's

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 //update
@@ -30,9 +31,37 @@ router.put("/:id", async (req, res) => {
 });
 
 //delete
-router.post("/delete", async (req, res) => {
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      try {
+        //when user delete  account user post deleted
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        return res.status(200).json({ message: "User has been deleted..." });
+      } catch (error) {
+        return res.status(500).json({ error });
+      }
+    } catch (error) {
+      return res.status(404).json({ message: "User not found!", error });
+    }
+  } else {
+    return res
+      .status(401)
+      .json({ message: "You can delete only your account!" });
+  }
+});
+
+//get user
+router.get("/:id", async (req, res) => {
   try {
-  } catch (error) {}
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json({ others });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 module.exports = router;
